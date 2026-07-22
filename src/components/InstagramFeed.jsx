3401c776d74bfed7a88c48cc53f98b11f7cab1f6
Sar0tra <kerhartf@gmail.com>
2026-07-22 08:30:21 +0200

    Upravy

--- src/components/InstagramFeed.jsx
+++ src/components/InstagramFeed.jsx
@@ -13,6 +13,7 @@ export const InstagramFeed = () => {
         }
     }, []);
 
+
     return (
         <section id="instagram" className="bg-zinc-950 text-white py-24 px-4 sm:px-6 lg:px-8 border-t border-zinc-900">
             <motion.div
